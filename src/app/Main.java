b69027d4f4b898a6598b92c2ca9855ff3257b757
Boichenko Oleksandr <alex.homerepair.www@gmail.com>
2024-05-13 07:00:22 +0100

    Converter °F to °C

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,20 @@
 package app;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("My converter degrees to farhrenheit");
+public class Main  {
 
-    }
+
+    private static double CONV_F_C = 0;
+
+
+    public static void main(String[] args) {
+            System.out.println("Converter °F to °C!!!");
+
+            double F = 32;
+            double celsius = convFrToCe(F);
+            System.out.println("Result is " + celsius +  "°.");
+        }
+
+        private static double convFrToCe(double F) {
+            return CONV_F_C = ((F - 32) * 5) / 9;
+        }
 }
